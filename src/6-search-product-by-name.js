619a8d7fa078644eb758ab1f7c3183f32e568f9f
JyojiTenguam <j.y.oji@hotmail.com>
2023-11-29 18:14:03 -0300

    Resolvendo requisito 06 do Supermercado Pirilampo

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
+const searchProductByName = (name) => {
   // Desenvolva seu código dentro dessa função...
+  let productByName = null;
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].productName === name) {
+      productByName = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+      };
+    }
+  }
+  return productByName;
 };
 
 module.exports = { searchProductByName };
