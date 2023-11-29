ea28caa05540ac1c392247a8ba9115d1615863d4
JyojiTenguam <j.y.oji@hotmail.com>
2023-11-29 08:54:39 -0300

    Resolvendo requisito 04 do Supermercado Pirilampo

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,17 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let productAvailable = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    estoque = stockProducts[index].quantityInStock;
+    if (estoque > 0 && estoque <= 10) {
+      productAvailable.push(
+        `${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`,
+      );
+    }
+  }
+  return productAvailable;
 };
 
 module.exports = { getLowStockProducts };
