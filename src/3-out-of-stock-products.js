2ee4b5136bf0c02b1ea30b602048a818a43ad692
JyojiTenguam <j.y.oji@hotmail.com>
2023-11-29 00:21:18 -0300

    Resolvendo requisito 03 do Supermercado Pirilampo

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
-};
+  const productNot = [];
 
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock === 0) {
+      productNot.push(stockProducts[index].productName);
+    }
+  }
+  return productNot;
+};
 module.exports = { getOutOfStockProducts };
