9bb4bb533070c8bf361f90423084bc0c643c15a4
JyojiTenguam <j.y.oji@hotmail.com>
2023-11-28 23:57:21 -0300

    Resolvendo requisito 02 do Supermercado Pirilampo

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  const productsUnique = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    productsUnique.push(stockProducts[index].productName);
+  }
+
+  return productsUnique;
 };
 
 module.exports = { getUniqueProductsName };
