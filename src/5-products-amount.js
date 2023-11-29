3c5a29138ec645d1c7e774184befc08212274029
JyojiTenguam <j.y.oji@hotmail.com>
2023-11-29 09:01:33 -0300

    Resolvendo requisito 05 do Supermercado Pirilampo

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let productAmount = 0;
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    productAmount += stockProducts[index].quantityInStock;
+  }
+  return productAmount;
 };
 
 module.exports = { getProductsAmount };
