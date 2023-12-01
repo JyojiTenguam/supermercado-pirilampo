2f4748e300450f0977cdb8d334828841e8c481a6
JyojiTenguam <j.y.oji@hotmail.com>
2023-11-30 22:33:47 -0300

    Resolvendo requisito 10 do Supermercado Pirilampo

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -2,4 +2,24 @@ const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
   // Desenvolva seu código dentro dessa função...
+  let productByName = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].nutritionalInfo.vitamins !== undefined) {
+      const vitaminsArray = Object.entries(stockProducts[index].nutritionalInfo.vitamins);
+      let arrayVit = [];
+      for (let vitIndex = 0; vitIndex < vitaminsArray.length; vitIndex += 1) {
+        arrayVit.push(`${vitaminsArray[vitIndex][0]} - ${vitaminsArray[vitIndex][1]}`);
+      }
+      productByName.push(
+        {
+          description: stockProducts[index].description,
+          formattedPrice: `R$ ${stockProducts[index].price}`,
+          vitaminsInformation: arrayVit,
+        },
+      );
+    }
+  }
+  return productByName;
+};
+console.log(getProductsRichInVitamin());
 module.exports = { getProductsRichInVitamin };
--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -10,7 +10,7 @@ const getProductsWithAllergyOrIntoleranceInfo = () => {
         {
           description: stockProducts[index].description,
           formattedPrice: `R$ ${stockProducts[index].price}`,
-          allergyMessage: `Pode conter: ${stockProducts[index].allergyOrIntolerance.join(' ')}`,
+          allergyOrIntoleranceMessage: `Pode conter: ${stockProducts[index].allergyOrIntolerance.join(' ')}`,
         },
       );
     }
